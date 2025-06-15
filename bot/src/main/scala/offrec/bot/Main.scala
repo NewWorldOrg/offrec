4cba65b33ba2e699c177dd0d76699aa2f8ff3f04
Ishikawa Ryuto <ishikawa-r@protonmail.com>
2025-06-15 22:40:48 +0900

    pingに対応

--- bot/src/main/scala/offrec/bot/Main.scala
+++ bot/src/main/scala/offrec/bot/Main.scala
@@ -3,6 +3,7 @@ package offrec.bot
 import cats.effect.{ExitCode, IO, IOApp}
 import com.typesafe.config.ConfigFactory
 import offrec.daemon.message_delete_daemon.MessageDeleteDamon
+import offrec.daemon.ping_daemon.PingDaemon
 import offrec.daemon.register_channel_command_daemon.RegisterChannelCommandDaemon
 import offrec.logging.Logger
 import scalikejdbc.config.DBs
@@ -25,6 +26,7 @@ object Main extends IOApp with Logger {
       _ <- setupDB
       registerChannelCommandDaemonFiber <- RegisterChannelCommandDaemon.task(discordBotToken).start
       messageDeleteDamonFiber <- MessageDeleteDamon.task(discordBotToken).start
+      pingDaemonFiber <- PingDaemon.task(discordBotToken).start
       _ <- registerChannelCommandDaemonFiber.join.guarantee {
         IO {
           logger.info("Shutting down register channel command daemon...")
@@ -33,6 +35,11 @@ object Main extends IOApp with Logger {
       _ <- messageDeleteDamonFiber.join.guarantee {
         IO {
           logger.info("Shutting down message delete daemon...")
+        }
+      }
+      _ <- pingDaemonFiber.join.guarantee {
+        IO {
+          logger.info("Shutting down ping daemon...")
         }
       }
     } yield ()).guarantee(closeDB).as(ExitCode.Success)
